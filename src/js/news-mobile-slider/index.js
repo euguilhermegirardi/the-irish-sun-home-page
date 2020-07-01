ecea67cec4c6d3cee9fba11530b0ad54f03ff392
Guilherme Girardi <girardi.gui@Guilhermes-MacBook-Pro.local>
2020-07-01 13:28:56 +0100

    Delete Slick Slider and add Tiny Slider Vanilla JS

--- src/js/news-mobile-slider/index.js
+++ src/js/news-mobile-slider/index.js
@@ -1,10 +1,2 @@
-$(document).ready(function(){
-   $('.cards-carousel').slick({
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      nextArrow: false,
-      prevArrow: false,
-      variableWidth: true,
-   });
- });
+
+
