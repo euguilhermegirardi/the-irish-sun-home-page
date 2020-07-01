b3137d0d67ca6cc5c569fe63335d22ae2d8c2687
Guilherme Girardi <girardi.gui@Guilhermes-MacBook-Pro.local>
2020-07-01 12:10:35 +0100

    Add Slick Slider

---
+++ src/js/news-mobile-slider/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+   $('.cards-carousel').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      nextArrow: false,
+      prevArrow: false,
+      variableWidth: true,
+   });
+ });
