a45e372ad7588da339e22b10d126db24d2223abb
Guilherme Girardi <girardi.gui@Guilhermes-MacBook-Pro.local>
2020-07-01 13:36:44 +0100

    Edit Tiny Slider

--- src/js/news-mobile-slider/index.js
+++ src/js/news-mobile-slider/index.js
@@ -1,2 +1,15 @@
+function slider() {
+   tns({
+      controls: false,
+      container: ".slider",
+      slideBy: "page",
+      items: 6,
+      mouseDrag: true,
+      swipeAngle: false,
+      speed: 400,
+      preventScrollOnTouch: "force",
+   });
+}
 
+slider();
 
