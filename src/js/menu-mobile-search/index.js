fb639464530b3bdae1a564f746fd5616ec97a8b8
Guilherme Girardi <girardi.gui@Guilhermes-MacBook-Pro.local>
2020-07-03 11:18:23 +0100

    Create search dropdown

---
+++ src/js/menu-mobile-search/index.js
@@ -0,0 +1,8 @@
+const dropdown = document.querySelector('.navbar__search');
+
+dropdown.addEventListener('click', function allDropdown() {
+  document.querySelector(".dropdown").classList.toggle("opened");
+
+  const active = document.querySelector('.navbar__search');
+  active.classList.toggle('active');
+});
